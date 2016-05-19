2368d24829e389f1eb550087b3479d416e2c0ccb
Davis <286636562@qq.com>
2016-05-19 20:22:37 +0800

    首页编写

--- app/src/main/java/com/davis/kangpinhui/fragment/ClassicFragment.java
+++ app/src/main/java/com/davis/kangpinhui/fragment/ClassicFragment.java
@@ -1,36 +1,1 @@
-package com.davis.kangpinhui.fragment;
-
-import android.support.v4.app.Fragment;
-import android.view.View;
-
-import com.davis.kangpinhui.fragment.base.BaseFragment;
-
-/**
- * Created by davis on 16/5/19.
- */
-public class ClassicFragment extends BaseFragment {
-    @Override
-    protected void initVariable() {
-
-    }
-
-    @Override
-    protected int setContentView() {
-        return 0;
-    }
-
-    @Override
-    protected void findViews(View view) {
-
-    }
-
-    @Override
-    protected void initData() {
-
-    }
-
-    @Override
-    protected void setListener() {
-
-    }
-}
+package com.davis.kangpinhui.fragment;import android.support.v4.app.Fragment;import android.view.View;import com.davis.kangpinhui.R;import com.davis.kangpinhui.fragment.base.BaseFragment;/** * Created by davis on 16/5/19. */public class ClassicFragment extends BaseFragment {    @Override    protected void initVariable() {    }    @Override    protected int setContentView() {        return R.layout.fragment_classic;    }    @Override    protected void findViews(View view) {    }    @Override    protected void initData() {    }    @Override    protected void setListener() {    }}
