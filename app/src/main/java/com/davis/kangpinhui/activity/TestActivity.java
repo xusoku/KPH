5f2ca0e8d1dfc45919f1f13c455b90159938a068
Davis <davis@azaleadeMacBook-Pro.local>
2016-07-03 16:35:41 +0800

    a
    a

---
+++ app/src/main/java/com/davis/kangpinhui/activity/TestActivity.java
@@ -0,0 +1,41 @@
+package com.davis.kangpinhui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.davis.kangpinhui.R;
+import com.davis.kangpinhui.activity.base.BaseActivity;
+
+public class TestActivity extends BaseActivity {
+
+    @Override
+    protected int setLayoutView() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    protected void initVariable() {
+
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    public void doClick(View view) {
+
+    }
+}
